0017297c0483cfe6b21847ca5f2cdaaccb4c9d25
Subham-Suar <subhamsuar911609@gmail.com>
2026-04-05 09:47:27 +0530

    ExpenseTracker

--- backend/server.js
+++ backend/server.js
@@ -50,8 +50,6 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
-app.options("*", cors());
-app.options("*", cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
