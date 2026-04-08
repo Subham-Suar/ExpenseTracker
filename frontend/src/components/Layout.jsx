5291fd2bd3f520b56977edc9edaad3124edafb09
Subham-Suar <subhamsuar911609@gmail.com>
2026-04-08 22:32:12 +0530

    ExpenseTracker

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -3,6 +3,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Sidebar from "./Sidebar";
 
+//Layout ----
 function Layout({ onLogout, user }) {
   const [sidebarCollapsed, setSidebarCollapsed] = useState(false);
   const [mobileSidebarOpen, setMobileSidebarOpen] = useState(false);
